679d76d485422870bfe15f16c46a99f126824486
Muhammad Yusri <yusri5865@gmail.com>
2025-01-29 17:27:27 +0700

    seccond commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import Admin from './admin/Admin'
 import './App.css'
-import Users from './users/Users'
 
 export default function App() {
   return (
     <>
-      <Users/>
+      <Admin/>
     </>
   )
 }
--- src/admin/Admin.jsx
+++ src/admin/Admin.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from "react-router";
 import SidebarLeft from "../sidebar-left/sidebarLeft";
 import SidebarRight from "../sidebar-right/SidebarRight";
 
-export default function Users() {
+export default function Admin() {
     return (
         <div className="container-xxl m-0 p-0 d-flex">
             <BrowserRouter>
